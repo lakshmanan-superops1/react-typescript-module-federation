b1c26c6a895aceecda9769fb474e1fd0ef748cf6
Lakshmanan <lakshmanan.arumugam@superops.ai>
2024-09-26 19:28:40 +0530

    File changes

--- app1/src/bootstrap.tsx
+++ app1/src/bootstrap.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root1") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <ChakraProvider>
